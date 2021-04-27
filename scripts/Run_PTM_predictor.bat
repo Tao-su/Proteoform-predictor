45848d294404b328adbe26f159938304305b0f88
Tao-su <63077152+Tao-su@users.noreply.github.com>
2021-04-27 14:28:31 +0800

    Update Run_PTM_predictor.bat

--- scripts/Run_PTM_predictor.bat
+++ scripts/Run_PTM_predictor.bat
@@ -7,5 +7,5 @@ for /f "usebackq tokens=1-6 delims=," %%a in ("parameters.csv") do (
       Set "_var5=%%e"
       Set "_var6=%%f"
 )
-python PTM_predictor_main.py %_var1% %_var2% %_var3% %_var4% %_var5% %_var6%
+python PTM_predictor.py %_var1% %_var2% %_var3% %_var4% %_var5% %_var6%
 pause
