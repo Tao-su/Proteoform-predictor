a881cbcf54c122a4c2d5e674842c4983fcffe205
Tao-su <63077152+Tao-su@users.noreply.github.com>
2021-08-08 20:00:18 -0500

    Update Append_XML.py

--- scripts/Append_XML.py
+++ scripts/Append_XML.py
@@ -15,7 +15,18 @@ def main():
 
     base_entry = base_tree.getroot()
     new_entry = new_tree.getroot()
-    base_entry.append(new_entry)
+
+    #remove copyright tag for the base xml
+    for entry in base_entry.findall('{http://uniprot.org/uniprot}copyright'):
+        base_entry.remove(entry)
+
+    # to remove the outer tag (uniport) for new xml
+    # take out the first entry which contains all protein related information and the second entry which is copyright node
+    # and append sequentially.
+    tobe_added_entry = new_entry[0]
+    base_entry.append(tobe_added_entry)
+    base_entry.append(new_entry[1])
+
     base_tree.write(args.result_name)
 if __name__ == '__main__':
     main()
